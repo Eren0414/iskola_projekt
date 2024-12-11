97e685975dab1d0b2911f19299f27d5093f9c59a
Eren0414 <kritof.jager.2006@gmail.com>
2024-12-11 13:25:43 +0100

    backend *

---
+++ Frontend/iskola_projekt/iskola_projekt/src/views/KartyakView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Kártyák</h2>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from 'axios';
+export default {
+  data(){
+    return {
+      urlApi: "http://localhost:8000/api/",
+      rows: [],
+    };
+  },
+  async mounted() {
+    this.getOsztalynevsor()
+  },
+  methods: {
+    async getOsztalynevsor(){
+      const url = `${this.urlApi}/getOsztalynevsor`
+      const request = await fetch(url);
+      this.rows = await request.json();
+    }
+  }
+}
+</script>
