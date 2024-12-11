01b76e7fdacd22d9c66c3f2338c31b5c0bbf6846
Eren0414 <kritof.jager.2006@gmail.com>
2024-12-11 13:47:28 +0100

    frontend helyrerakas

---
+++ Frontend/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+export default {};
+</script>
+
+<template>
+  <div class="container-fluid my-border my-container">
+    <h1>Iskola</h1>
+
+    <nav class="my-border p-4">
+      <RouterLink to="/">Home</RouterLink> |
+      <RouterLink to="/kartyak">Kártyák</RouterLink>
+    </nav>
+
+    <div class="my-border p-2">
+      <RouterView />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
